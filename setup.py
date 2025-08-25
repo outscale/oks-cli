c865ca6ec590edddc3a77c3b8d657d7a5fcec297
Yurii Kasper <yurii.kasper@outscale.com>
2025-08-25 17:30:57 +0200

    🔖 release: prepare oks-cli version output due to the new release version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="oks-cli",
-    version="1.14",
+    version="1.15",
     packages=['oks_cli'],
     author="Outscale SAS",
     author_email="opensource@outscale.com",
