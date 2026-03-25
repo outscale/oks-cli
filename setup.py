3fda32b522cafc2e6955a04bb912a4fe33b09c77
Romain Demeure <romain.demeure@outscale.com>
2026-03-25 10:23:16 +0100

    🐛 fix: add encoding=utf-8 to long_description in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=['oks_cli'],
     author="Outscale SAS",
     author_email="opensource@outscale.com",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(), 
     long_description_content_type="text/markdown",
     include_package_data=True,
     license="BSD",
